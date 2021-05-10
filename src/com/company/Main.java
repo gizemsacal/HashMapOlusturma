3bc310d9db2190634731b42c9c279ace244414a3
gizem <gizemsacal@gmail.com>
2021-05-10 15:58:48 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.HashMap;
+
+class Main {
+    public static void main(String[] args) {
+
+        // create a hashmap
+        HashMap<String, Integer> languages = new HashMap<>();
+
+        // add elements to hashmap
+        languages.put("Java", 8);
+        languages.put("JavaScript", 1);
+        languages.put("Python", 3);
+        System.out.println("HashMap: " + languages);
+    }
+}
